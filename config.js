1e84a09404b083a5d89c37d05155e0d3685544f2
HalloPeanut <106302012+HalloPeanut@users.noreply.github.com>
2023-11-19 17:37:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ const eventName = 'HỒ NGỌC BẢO';
 const titleCard = 'TẶNG BẠN NÈ';
 const contentCard = 'CHÚC BẠN NHỎ CỦA MÌNH 20/11 DUI DẺ, MÃI EO GIÁO DIÊN MẦM NON';
 
-const giftImage = 'bao.jpg';
+const giftImage = 'bao1.jpg';
 const base64 = '';
 const giftImageBase64 = "data:image/png;base64, " + base64;
